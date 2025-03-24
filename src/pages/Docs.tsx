e614a6d6032cbf4a227018efa87131af058e8594
Marcell <46452318+Heriloga@users.noreply.github.com>
2025-03-24 13:53:00 +0100

    fix: add missing key prop

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -5,8 +5,8 @@ import { examples } from "../components/CodeBlock/examples";
 function Docs(): ReactElement {
   return (
     <div className="mt-64 h-screen">
-      {examples.map((example) => (
-        <div className="bg-white p-5 mb-5">
+      {examples.map((example, exampleIdx) => (
+        <div className="bg-white p-5 mb-5" key={exampleIdx}>
           <h1 className="text-xl text-center">{example.title}</h1>
           <p className="text-md p-4">{example.desc}</p>
 
